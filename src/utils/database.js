2918bce02e1b9f94aa6cc0d12f1b6730653c05fd
mehmoodtkxel <mehmood.hussain@tkxel.com>
2023-12-20 16:43:16 +0500

    db connection updated

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
 });
 
 module.exports = { sequelize };
