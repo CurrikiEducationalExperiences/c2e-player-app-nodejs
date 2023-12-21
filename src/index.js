1a728b1d44e4eba8f2e65aaf511c22d1a90c57c8
mehmoodtkxel <mehmood.hussain@tkxel.com>
2023-12-21 10:27:10 +0500

    lti prefix removed, shifted erver to lti. whitelisted the public endpoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const path = require("path");
-const swaggerUi = require("swagger-ui-express");
 const lti = require("ltijs").Provider;
 const Database = require("ltijs-sequelize");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const routes = require("./routes/routes");
 const { setRouter } = require("./routes/api");
@@ -23,50 +21,6 @@ const db = new Database(
   }
 );
 
-/////////////////// EXPRESS APP ///////////////////
-const app = express();
-app.server = http.createServer(app);
-
-/////////////////// BODY PARSER ///////////////////
-app.use(bodyParser.urlencoded({ extended: false }));
-////////////// PARSE application/json /////////////
-app.use(
-  bodyParser.json({
-    limit: "2000kb",
-  })
-);
-
-// cors
-app.use(
-  cors({
-    maxAge: 2628000, //  Access-Control-Max-Age: 1 month
-  })
-);
-
-/////////////////// SWAGGER UI ///////////////////
-if (process.env.ENV === "development") {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-}
-
-/////////////// SET PUBLIC ROUTER ////////////////
-setRouter(app);
-
-/////// GLOBAL ERROR LANDLER AS MIDDLEWARE //////
-app.use((err, req, res, next) => globalErrorHandler(err, req, res, next));
-
-////////////// EXPRESS APP SERVER ///////////////
-app.server.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log(
-    `Started server on => http://localhost:${app.server.address().port}`
-  );
-  console.log(
-    `Docs available on => http://localhost:${
-      app.server.address().port
-    }/api-docs`
-  );
-});
-
-////////////////// LTI SETUP //////////////////
 lti.setup(
   process.env.LTI_KEY,
   {
@@ -74,46 +28,84 @@ lti.setup(
   },
   {
     tokenMaxAge: false,
-    staticPath: path.join(__dirname, '../public'), //  Path to static files
+    staticPath: path.join(__dirname, "../public"), // Path to static files
     cookies: {
       secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: '', // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      sameSite: "", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
     },
     devMode: true, // Set DevMode to true if the testing platform is in a different domain and https is not being used
-    dynRegRoute: '/register', // Setting up dynamic registration route. Defaults to '/register'
+    dynRegRoute: "/register", // Setting up dynamic registration route. Defaults to '/register'
     dynReg: {
-      url: 'http://localhost:3000', // Tool Provider URL. Required field.
-      name: 'Tool Provider', // Tool Provider name. Required field.
-      logo: 'http://localhost:3000/logo512.png', // Tool Provider logo URL.
-      description: 'Tool Description', // Tool Provider description.
-      redirectUris: ['http://localhost:3000/launch'], // Additional redirection URLs. The main URL is added by default.
+      url: "http://localhost:3000", // Tool Provider URL. Required field.
+      name: "Tool Provider", // Tool Provider name. Required field.
+      logo: "http://localhost:3000/logo512.png", // Tool Provider logo URL.
+      description: "Tool Description", // Tool Provider description.
+      redirectUris: ["http://localhost:3000/launch"], // Additional redirection URLs. The main URL is added by default.
       customParameters: { key: "value" }, // Custom parameters.
       autoActivate: true, // Whether or not dynamically registered Platforms should be automatically activated. Defaults to false.
     },
   }
 );
-
 lti.onConnect(async (token, req, res) => {
   return res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
 lti.onDeepLinking(async (token, req, res) => {
   if (req.query.c2eId) {
-    return lti.redirect(res, "/lti/play", { newResource: true });
+    return lti.redirect(res, `/play`, { newResource: true });
   } else {
-    return lti.redirect(res, "/lti/deeplink", { newResource: true });
+    return lti.redirect(res, "/deeplink", { newResource: true });
   }
 });
 
-//////////////// SET LTI ROUTES //////////////
-app.use("/lti", lti.app);
+/////////////////// WHITELISTING ROUTES ///////////////////
+lti.whitelist(new RegExp(/^\/api\/v1/), {
+  route: new RegExp(/^api\/v1/),
+  method: "post",
+});
+lti.whitelist(new RegExp(/^\/api\/v1/), {
+  route: new RegExp(/^api\/v1/),
+  method: "get",
+});
+lti.whitelist(new RegExp(/^\/api\/v1/), {
+  route: new RegExp(/^api\/v1/),
+  method: "patch",
+});
+lti.whitelist(new RegExp(/^\/api\/v1/), {
+  route: new RegExp(/^api\/v1/),
+  method: "delete",
+});
+lti.whitelist({ route: "/platform/register", method: "post" });
+
+/////////////////// BODY PARSER ///////////////////
+lti.app.use(bodyParser.urlencoded({ extended: false }));
+
+////////////// PARSE application/json /////////////
+lti.app.use(
+  bodyParser.json({
+    limit: "2000kb",
+  })
+);
+
+////////////////////// CORS //////////////////////
+lti.app.use(
+  cors({
+    maxAge: 2628000, //  Access-Control-Max-Age: 1 month
+  })
+);
+
+/////////////////// SWAGGER UI ///////////////////
+if (process.env.ENV === "development") {
+  lti.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
+setRouter(lti.app);
 lti.app.use(routes);
 
-///////////// LTI SETUP FUNCTION ///////////
+/////// GLOBAL ERROR LANDLER AS MIDDLEWARE //////
+lti.app.use((err, req, res, next) => globalErrorHandler(err, req, res, next));
+
 const setup = async () => {
-  await lti.deploy({ serverless: true });
+  await lti.deploy({ port: process.env.PORT });
 };
 setup();
-
-module.exports = { app };
-
+module.exports = { app: lti.app };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = require('./user');
 const accountsRouter = require('./accounts');
-const adminRouter = require('./admin');
 
 const router = express.Router();
 
@@ -9,7 +8,6 @@ const setRouter = (app) => {
   app.use('/api/v1', router);
   router.use(`/users`, userRouter);
   router.use(`/accounts`, accountsRouter);
-  router.use('/admin', adminRouter);
 };
 
 module.exports = { setRouter };
