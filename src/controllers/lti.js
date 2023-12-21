bf90212f98f4d3cbaf9d3052d96d22a5f2022abf
mehmoodtkxel <mehmood.hussain@tkxel.com>
2023-12-21 19:51:36 +0500

    updated the controller folder

--- src/controllers/lti.js
+++ src/controllers/lti.js
@@ -10,31 +10,37 @@ const { ltiService } = require("../service/lti");
 class ltiController {
   static async grade(req, res) {
     const response = await ltiService.grade(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
-  
+
   static async members(req, res) {
     const response = await ltiService.members(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
-  
+
   static async deeplink(req, res) {
     const response = await ltiService.deeplink(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
-  
+
   static async play(req, res) {
     const response = await ltiService.play(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
 
   static async info(req, res) {
     const response = await ltiService.info(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
 
   static async resources(req, res) {
     const response = await ltiService.resources(req, res);
+    if (response.code === 200) return res.send(response.data);
     return res.status(response.code).send(response.data);
   }
 
@@ -93,51 +99,57 @@ class ltiController {
   }
 
   static async xapi(req, res) {
-    var platformSettings = await PlatformSetting.findOne({ where: {lti_client_id: res.locals.token.clientId}});
+    var platformSettings = await PlatformSetting.findOne({
+      where: { lti_client_id: res.locals.token.clientId },
+    });
     if (!platformSettings) {
-        return res.status(400).send({
-            status: 400,
-            error: "No matching platform settings found",
-            details: {
-              description: "Your LTI authentication information doesn't match any existing platform settings in the C2E player",
-              message: "No matching platform settings found"
-            }
-          });
+      return res.status(400).send({
+        status: 400,
+        error: "No matching platform settings found",
+        details: {
+          description:
+            "Your LTI authentication information doesn't match any existing platform settings in the C2E player",
+          message: "No matching platform settings found",
+        },
+      });
     }
 
-    if (!req.body.id || !req.body.verb){
-        return res.status(400).send({
-            status: 400,
-            error: "No xAPI statement provided",
-            details: {
-              description: "The request params provided do not match a valid xAPI statement format",
-              message: "No xAPI statement provided"
-            }
-        });
+    if (!req.body.id || !req.body.verb) {
+      return res.status(400).send({
+        status: 400,
+        error: "No xAPI statement provided",
+        details: {
+          description:
+            "The request params provided do not match a valid xAPI statement format",
+          message: "No xAPI statement provided",
+        },
+      });
     }
 
     const params = {
-        statement: JSON.stringify(req.body),
-        email: platformSettings.cee_licensee_id,
-        secret: platformSettings.cee_secret_key,
+      statement: JSON.stringify(req.body),
+      email: platformSettings.cee_licensee_id,
+      secret: platformSettings.cee_secret_key,
     };
 
     const xapiServiceUrl = `${platformSettings.cee_provider_url}/xapi`;
-    await axios.post(xapiServiceUrl, params)
-    .then(async (response) => {
+    await axios
+      .post(xapiServiceUrl, params)
+      .then(async (response) => {
         return res.send(response.data);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         return res.status(400).send({
-            status: 400,
-            error: "Failed to send  xAPI statement to service provider",
-            details: {
-              description: "Failed to send  xAPI statement to service provider. Check your integration settings",
-              message: "Failed to send  xAPI statement to service provider"
-            }
+          status: 400,
+          error: "Failed to send  xAPI statement to service provider",
+          details: {
+            description:
+              "Failed to send  xAPI statement to service provider. Check your integration settings",
+            message: "Failed to send  xAPI statement to service provider",
+          },
         });
-    });
+      });
   }
 
   static async registerPlatform(req, res) {
